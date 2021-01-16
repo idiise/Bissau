f30894547b4513b122545d9ddad617fb71e8c9f2
Idrissa dabo <ididabo@gmail.com>
2021-01-16 22:38:50 +0000

    nouveau script

---
+++ script de tabulation.R
@@ -0,0 +1,114 @@
+
+# import packages ---------------------------------------------------------
+
+library(readxl)
+library(stringi)
+library(tidyverse)
+library(tidyquant)
+library(writexl)
+library(haven)
+library(labelled)
+library(gmodels)
+library(janitor)
+library(rlist)
+library(openxlsx)
+# Import Dataset ----------------------------------------------------------
+
+GNB1 <-  read.csv("GNB_MFI_Dataset_September_2020.csv")
+GNB2 <- read_xlsx("GNB_MFI_Dataset_September_2020_v2.xlsx")
+marche <- read_xlsx("Marche_Bissau.xlsx")
+nomfichier2 <- read_xlsx("nomfichier2.xlsx")
+mfi_bissau <- read_xlsx("mfi_bissau.xlsx")
+spss <- read_sav("data_mfi_beta.sav")
+spss <- to_factor(spss)
+d <- var_label(spss)
+d <- pluck(d)
+d <- as.data.frame(do.call(rbind,d))
+d <- d %>% rownames_to_column()
+
+# Assortiment -------------------------------------------------------------
+
+mfi_bissau <- mutate_at(mfi_bissau, vars(contains("UOASoldGroup_FCer-fc")), funs(factor))
+mfi_bissau <- mutate_at(mfi_bissau, vars(contains("UOASoldGroup_FCer-fc")), ~recode_factor(.,"0"="Non", "1"="Oui"))
+
+Assortiment1 <- list()
+for (i in 1:length(colonne)) {
+  if(str_detect(colonne[i], "UOASoldGroup_FCer-fc") )
+    Assortiment1 <- append(Assortiment1, colonne[i])
+  
+}
+Assortiment2 <- list()
+for (i in 1:length(colonne)) {
+  if(str_detect(colonne[i], "UOASoldGroup_FCer-fc") )
+    Assortiment2 <- append(Assortiment2, colonne[i])
+  
+}
+
+df_list <- map(Assortiment, ~ tableau(mfi_bissau, !!sym(.x)))
+names(df_list) <- Assortiment
+list2env(df_list, envir = .GlobalEnv)
+
+nom <- map(disponibilite,~str_replace(.x, "-","."))
+d[which(d$rowname%in%nom),]
+
+sheet_names <- excel_sheets("Disponibilité/Assortiment.xlsx")
+nb_sheets <-  length(sheet_names)
+
+base_vide <- NULL
+for (i in 1:nb_sheets){
+  name <- paste0("Produits_habituellement_vendus__",sheet_names[i]) #r?cup?re le nom de la feuille
+  data <- read_excel("Disponibilité/Assortiment.xlsx", sheet = i)# stock les donn?es
+  data$modalite=name # ajout d'une variable region
+  base_vide<- bind_rows(base_vide,data)     
+  rm(name,data) 
+} 
+
+# Disponibilté ------------------------------------------------------------
+
+
+# Prix --------------------------------------------------------------------
+
+
+# Résilience --------------------------------------------------------------
+
+
+# Service -----------------------------------------------------------------
+
+
+# Infrastructure ----------------------------------------------------------
+
+
+# Competition -------------------------------------------------------------
+
+
+# Qualité -----------------------------------------------------------------
+
+
+# Acces et Protection -----------------------------------------------------
+
+
+
+
+# Fonction de tabulation --------------------------------------------------
+
+tableau <- function(d,colonne){
+  d <- mfi_bissau %>% tabyl(MktNametext, {{colonne}},show_na = TRUE) %>% 
+    adorn_totals(where = c("col")) %>%
+    adorn_percentages(denominator = "row") %>% 
+    adorn_pct_formatting(digits = 0)
+  d <- d %>% mutate(
+    Admin2name = VLOOKUP(.lookup_values = MktNametext,.data = marche,.lookup_column = MarketName,.return_column = Adm2Name)
+  )
+  d <- d %>% mutate(
+    Admin1name = VLOOKUP(.lookup_values = MktNametext,.data = marche,.lookup_column = MarketName,.return_column = Adm1Name)
+  )
+  d <- d %>% relocate(Admin2name,.before = MktNametext)
+  d <- d %>% relocate(Admin1name,.before = Admin2name)
+  
+}
+
+
+# Fonction de sauvegarde --------------------------------------------------
+
+
+
